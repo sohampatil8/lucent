c30ced82c4de8c0adc8c4403a81cf845248f21b9
soham <sohamsatyajitpatil@gmail.com>
2026-02-26 11:03:15 +0530

    added docker_lucent folder

--- docker_lucent/client/src/lib/api.js
+++ docker_lucent/client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://172.17.0.3:5000/api",
 });
 
 api.interceptors.request.use((config) => {
